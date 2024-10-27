c9ba8d5d7072cc5e2484eed7e97719f640d87d19
Alexandre Silva <asilva1604@outlook.com>
2024-10-27 14:47:25 +0000

    start application layer

--- src/application_layer.c
+++ src/application_layer.c
@@ -6,6 +6,10 @@
 #include "link_layer.h"
 #include <string.h>
 
+#define CStart 1
+#define TSize 0
+#define TName 1
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
@@ -21,6 +25,48 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     link.nRetransmissions = nTries;
     link.timeout = timeout;
     llopen(link);
+
+    //BEGIN
+
+    if (link.role == LlTx) {
+        FILE *file = fopen(filename, "r");
+
+        if (file == NULL ){
+            perror("Error opening file, not found...");
+            return;
+        }
+
+        char buffer[MAX_PAYLOAD_SIZE]; //is the size here correct?
+
+        //send control packet
+        buffer[0] = CStart;
+        //write the rest of packet...
+
+        while (fgets(buffer, sizeof(buffer), file)) {
+            //do something
+        } 
+
+        fclose(file);
+        //send control packet
+
+        return;
+    } else {
+        FILE *file = fopen(filename, "w");
+
+        if (file == NULL) {
+            perror("Error writing to or creating file...");
+            return;
+        }
+        char buffer[MAX_PAYLOAD_SIZE];
+        while (TRUE) {
+            int read = llread(&buffer);
+
+        }
+
+
+        fclose(file);
+        return;
+    }
 
     if (strncmp(role,"tx", 2) == 0) {
         unsigned char rr[6] = {"AAAAA"};
